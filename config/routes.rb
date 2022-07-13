923a8d167130399e8076aafd6041ce71514ba2aa
carson mulligan <carsonmulligan45@gmail.com>
2022-07-13 16:39:39 -0500

    teapee

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'timers/index'
+  get 'timers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

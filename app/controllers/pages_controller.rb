046927fd69162c3494f089f8980aaae8611ea137
carson mulligan <carsonmulligan45@gmail.com>
2022-07-13 21:17:55 -0500

    added timer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def show
+    distance_in_seconds = (2.days.since(Time.now) - Time.zone.now).round
+
+    blob = GifCountdown.generate(distance_in_seconds: distance_in_seconds, duration: 10)
+    send_data blob, type: 'image/gif', disposition: 'inline'
+  end
 end
